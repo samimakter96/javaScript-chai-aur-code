86d6a6ca4bf977fec528464584acef6e4bbc356c
samim akter <samimakter9641@gmail.com>
2024-08-23 21:24:48 +0530

    datatype conversion

---
+++ Basics/03_DatatypeConversion.js
@@ -0,0 +1,53 @@
+// let score = "33abc";
+
+// console.log(typeof score);
+
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+//  1 => true; 0 => false
+//  "" => false
+// "samim" => true
+
+
+// let someNumber = 33;
+
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// Operations
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+
+// let str1 = "hello";
+// let str2 = " samim";
+
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
