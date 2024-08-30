fb36b5b241d455b647af71aaf62af725fa0dda5a
samim akter <samimakter9641@gmail.com>
2024-08-30 21:35:07 +0530

    truthy falsy

---
+++ control-flow/03-truthy-falsy.js
@@ -0,0 +1,48 @@
+// const userEmail = 'samim@google.com';
+
+
+// if (userEmail) {
+//   console.log("Got user Email")
+// } else {
+//   console.log("Don't have user Email");
+// }
+
+
+/*
+falsy values :
+false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+truthy values:
+every thing truthy value except falsy value. surprise truthy values - "0", "false", "  ", [], {}, function() {}
+*/
+
+// const userEmail = []
+// if (userEmail.length === 0) {
+//   console.log("Array is empty")
+// }
+
+
+// const emptyObj = {}
+
+// if (Object.keys(emptyObj).length === 0) {
+//   console.log("Object is empty")
+// }
+
+
+// Nullish Coalescing Operator (??): null undefined
+
+let val;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 20
+
+console.log(val1)
+
+
+// Ternary Operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100;
+iceTeaPrice <= 80 ? console.log('less than 80') : console.log('more than 80');
